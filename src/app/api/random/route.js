9cc6733025b26c7018ce3865d045a0f7b6c21a33
anukultyagi <anukultyagi321@gmail.com>
2025-01-10 18:28:27 +0530

    added cors policy

--- src/app/api/random/route.js
+++ src/app/api/random/route.js
@@ -1,10 +1,14 @@
+import cors, { runMiddleware } from '@/app/lib/cors';
 import quotes from '../../../data/quotes.json'
 
-export async function GET() {
 
+export async function GET(re, res) {
+
+    await runMiddleware(req, res, cors);
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
 
-    return Response.json(randomQuote);
+    return Response.status(200).json(randomQuote);
+
 
 } 
